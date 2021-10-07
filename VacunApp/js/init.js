3d5b8f94cdcebfafe4e3fa8e8e5ba11ea71e5eb2
Yiduard Andres Bolivar Ruiz <yiduard.bolivar@panamericana.local>
2021-10-07 13:29:01 -0500

    Se crean funciones para completar el esquema de vacunacion segun la fecha y laboratorio farmaceutico

--- VacunApp/js/init.js
+++ VacunApp/js/init.js
@@ -32,20 +32,33 @@ $(document).ready(function() {
 });
 
 //funcion para fecha
+
 $(function() {
     $("#farmaceutica").change(function() {
-        if ($(this).val() === "janssen") {
-            $("#2da_dosis").prop("disabled", true);
-            $("#esquema").prop("checked","checked")
-            $("#2da_dosis").val("No Aplica");
-        } else {
-            $("#2da_dosis").prop("disabled", false);
-            
-            
-        }
+        $("#esquema").prop("checked", ""); // esquema incompleto
+        $("#1er_dosis").val(""); // Deje en blanco la fecha 1
+        $("#2da_dosis").val(""); // Deje en blanco la fecha 2
+        $("#1er_dosis").prop("disabled", false); // active solo fecha 1
+        $("#2da_dosis").prop("disabled", true); // desactive la fecha 2
     });
 });
-
-
-
-// $("#esquema").prop("checked","checked")
+$("#1er_dosis").change(function() {
+    $("#2da_dosis").val(""); // Deje en blanco la fecha 2
+    if ($("#farmaceutica").val() === "janssen") {
+        $("#2da_dosis").prop("disabled", true); // desactive la fecha 2
+        $("#esquema").prop("checked", "checked"); // esquema completo
+        M.toast({ html: "Esquema de Vacunacion Completado", classes: "rounded" });
+    } else if ($("#farmaceutica").val() !== "janssen") {
+        $("#2da_dosis").prop("disabled", false); // active la fecha 2
+        $("#esquema").prop("checked", ""); // esquema incompleto
+    }
+});
+$("#2da_dosis").change(function() {
+    if ($("#1er_dosis").val().length == 0) {
+        M.toast({ html: "No hay fecha de la Primera Dosis", classes: "rounded" });
+        $("#2da_dosis").val(""); // Deje en blanco la fecha 2
+    } else {
+        $("#esquema").prop("checked", "checked"); // esquema completo
+        M.toast({ html: "Esquema de Vacunacion Completado", classes: "rounded" });
+    }
+});
