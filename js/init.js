8362119d1e4cb25e3be9b3c548d1c68cbc6e0039
jucetina <juliethcetina@gmail.com>
2021-10-10 12:30:10 -0500

    Se corrigen los errores encontrados y se agregan algunos estilos en los header y en los navbar

--- js/init.js
+++ js/init.js
@@ -7,22 +7,10 @@
     }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-
-//SELECT
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-});
-
 // Or with jQuery
 
 $(document).ready(function() {
     $('select').formSelect();
-});
-//DATE
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, options);
 });
 
 // Or with jQuery
